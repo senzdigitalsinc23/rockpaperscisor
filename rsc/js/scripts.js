d5f3788ee5a70f65539da39ab09daecb68c28325
Douglas Senzu <senzdigitalsinc23@gmail.com>
2024-04-21 14:18:31 +0000

    Completed playRound Function
    Completely coded the playRound function

--- rsc/js/scripts.js
+++ rsc/js/scripts.js
@@ -7,3 +7,33 @@ function getComputerChoice() {
     return computerSelection;
 }
 
+let compScore = playerScore = 0;
+
+function playRound(playerSelection, computerSelection) {
+
+    computerSelection = computerSelection.toLowerCase();    
+
+    if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        playerScore += 1;
+        console.log('You Won! Rock beats Scissors')
+    }
+    else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        playerScore += 1;
+        console.log('You Won! Scissors beat Paper')
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        playerScore += 1;
+        console.log('You Won! Paper beats Rock')
+    } else if (computerSelection === 'rock' && playerSelection === 'scissors') {
+        compScore += 1;
+        console.log('You Lose! Rock beats Scissors')
+    }
+    else if (computerSelection === 'scissors' && playerSelection === 'paper') {
+        compScore += 1;
+        console.log('You Lose! Scissors beats Paper')
+    } else if (computerSelection === 'paper' && playerSelection === 'rock') {
+        compScore += 1;
+        console.log('You Lose! Paper beats Rock')
+    }else if (playerSelection === computerSelection) {
+        console.log("Game Draw");
+    }
+}
